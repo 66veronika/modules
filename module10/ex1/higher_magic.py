da673b33d99a2df7d9d5ffcdfa150c78a53c379f
Veronika Skopova <vskopova@c3r7s2.42prague.com>
2026-05-08 03:46:33 +0200

    .

--- module10/ex1/higher_magic.py
+++ module10/ex1/higher_magic.py
@@ -2,10 +2,16 @@ from typing import Callable
 
 
 def spell(target: str, power: int) -> str:
-
+    pass
 
 def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
+    def combined(target: str, power: int) -> tuple:
+        return (
+            spell1(target, power),
+            spell2(target, power)
+        )
 
+    return combined
 
 def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
 
